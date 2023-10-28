d771a2a67e032d05bcd3c444522a4fab36cb299d
ezio.junior <ezio@databoxsistemas.com.br>
2023-10-28 20:59:56 -0300

    ok

--- src/app/demo/components/apps/domain/entities/user-entity.ts
+++
@@ -1,6 +0,0 @@
-import { DomainEntity } from './base/domain-entity';
-export class UserEntity extends DomainEntity {
-  channel?:string;
-  email?: string;
-  password?: string;
-}
--- src/app/demo/components/apps/environments/environmentTest.ts
+++
@@ -1,7 +0,0 @@
-// import { version } from '../assets/assets';
-export const environment = {
-    production: false,
-    token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImpvaG5AZG9lLmNvbS5iciIsImlkIjoxLCJyb2xlIjoiYWRtaW4iLCJleHAiOjE2ODc4OTI0MzgsImlhdCI6MTY4Nzg4ODgzOH0.faZzQ2rCkHutk99Y40Z5QEHMCrr1Fg_NpAzYUZMCRSg',
-    apiUrlAdm: 'https://box-empresa-api.com/api/v1',
-  };
-  
